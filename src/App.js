101a7e6a7e57b5e8d8c363f1101f826cfc0bf96f
Muhammad Adnan <adnan.programmer@gmail.com>
2020-12-02 22:44:44 +0100

    initial code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>MY FIRST PWA APPLICATION</h1>
+
+      <h3>Global Software Development</h3>
+    </div>
+  );
+}
+
+export default App;
